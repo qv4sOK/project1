77176f5d870236cdc6b86961d95751c1658a0cb2
kant <kant@DESKTOP-JOH00M9>
2022-03-23 15:19:40 +0300

    Добавьте файлы проекта.

---
+++ project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+   
+        static void Main(string[] args)
+        {
+        string[] array = new string[1];
+           
+            {
+                Console.WriteLine("Hello{0}.Welcome to our event");
+                
+            }
+        }
+    }
+
